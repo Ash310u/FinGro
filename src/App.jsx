5649d4e1d471482ce254d758b4acac5a48d17267
Half-Blood Prince <107404699+Ash310u@users.noreply.github.com>
2025-03-20 17:22:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AssistantSearch from './components/AssistantSearch';
 import Lists from './components/Lists';
 
+/**
+ * Copyright 2025 Ashutosh Saha
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 const App = () => {
     return (
       <Router>
